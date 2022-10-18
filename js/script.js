22752c5283324481d3cee0536ea14e6a4150be66
cordoba92 <112353439+cordoba92@users.noreply.github.com>
2022-10-18 22:05:06 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -29,35 +29,7 @@ navToggle.addEventListener("click", () => {
 
 
 
-// Variables
-const baseDeDatos = [
-  {
-      id: 1,
-      nombre: 'Patata',
-      precio: 1,
-      imagen: 'patata.jpg'
-  },
-  {
-      id: 2,
-      nombre: 'Cebolla',
-      precio: 1.2,
-      imagen: 'cebolla.jpg'
-  },
-  {
-      id: 3,
-      nombre: 'Calabacin',
-      precio: 2.1,
-      imagen: 'calabacin.jpg'
-  },
-  {
-      id: 4,
-      nombre: 'Fresas',
-      precio: 0.6,
-      imagen: 'fresas.jpg'
-  }
- 
 
-];
 
 let carrito = [];
 const divisa = '$';
